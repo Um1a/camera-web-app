205587e988e41a594ad10f4959327d0d2fe9e643
Um1a <s1314148@live.hkmu.edu.hk>
2025-02-21 12:32:32 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,68 @@
 var frontCamera = false;
 var currentStream;
+
+// Define constants
+// Get the element in the document with id="camera-view", "camera-device", "photo-display", "take-photo-button" and "front-camera-button"
 const
     cameraView = document.querySelector("#camera-view"),
     cameraDevice = document.querySelector("#camera-device"),
     photoDisplay = document.querySelector("#photo-display"),
     takePhotoButton = document.querySelector("#take-photo-button");
     frontCameraButton = document.querySelector("#front-camera-button");
-function cameraStart(){
-    if(typeof currentStream !== 'undefined'){
-        currentStream.getTracks().forEach(track =>{
+
+// Access the device camera and stream to cameraDevice
+function cameraStart() {
+    // Stop the video streaming before access the media device
+    if (typeof currentStream !== 'undefined') {
+        currentStream.getTracks().forEach(track => {
             track.stop();
         });
     }
-    var constraints = {video: {facingMode: (frontCamera? "user" : "environment")},audio :false};
+
+// Set constraints for the video stream
+// If frontCamera is true, use front camera
+// Otherwise, user back camera
+// "user" => Front camera
+// "environment" => Back camera
+    var constraints = { video: { facingMode: (frontCamera? "user" : "environment") }, audio: false };
+    
+    // Access the media device, camera in this example
     navigator.mediaDevices
         .getUserMedia(constraints)
-        .then(function(stream){
+        .then(function(stream) {
             currentStream = stream;
             cameraDevice.srcObject = stream;
         })
-        .catch(function(error){
-            console.error("Error happened.",error);
+        .catch(function(error) {
+            console.error("Error happened.", error);
         });
 }
 
-takePhotoButton.onclick = function(){
+// If takePhotoButton clicked => Take and display a photo
+takePhotoButton.onclick = function() {
     cameraView.width = cameraDevice.videoWidth;
     cameraView.height = cameraDevice.videoHeight;
-    cameraView.getContext("2d").drawImage(cameraDevice,0,0);
+    cameraView.getContext("2d").drawImage(cameraDevice, 0, 0);
     photoDisplay.src = cameraView.toDataURL("image/webp");
     photoDisplay.classList.add("photo-taken");
 };
 
-frontCameraButton.onclick = function(){
+// If Front/Back camera is click => Change to front/back camera accordingly
+frontCameraButton.onclick = function() {
+    // Toggle the frontCamera variable
     frontCamera = !frontCamera;
-    if(frontCamera){
-        frontCameraButton.textContent="Back Camera";
-    }else{
-        frontCameraButton.textContent="Front Camera";
+    // Setup the button text
+    if (frontCamera) {
+        frontCameraButton.textContent = "Back Camera";
     }
+    else {
+        frontCameraButton.textContent = "Front Camera";
+    }
+    // Start the video streaming
     cameraStart();
 };
-window.addEventListener("load",cameraStart);
 
+// Start the camera and video streaming when the window loads
+// 1st parameter: Event type
+// 2nd parameter: Function to be called when the event occurs
+window.addEventListener("load", cameraStart);
